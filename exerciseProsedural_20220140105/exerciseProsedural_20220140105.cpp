226704df1c6d05288903c9588ce4a657f4d90b78
giant-paw <giantpawbusiness@gmail.com>
2023-03-27 02:56:15 +0700

    mencoba algo baru

--- exerciseProsedural_20220140105/exerciseProsedural_20220140105.cpp
+++ exerciseProsedural_20220140105/exerciseProsedural_20220140105.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int a[20];
+int a[1000];
+int n;
+string nama;
 
-double rerata(double a, double b) {
-	return (a + b) / 2;
+
+struct student {
+	string nama;
+	double nilMat, nilFis;
+};
+
+double rerata (int i, student mahasiswa[]) {
+	return ((mahasiswa[i].nilMat) + (mahasiswa[i].nilFis) / 2);
 }
 
 int main() {
-
+	
 }
+
+
+//double rerata(double a, double b) {
+//	return (a + b) / 2;
+//}
+//
+//
+//string status(double rata, double nil) {
+//	if (rata >= 70 || nil > 80) {
+//		return "Diterima";
+//	}
+//	else {
+//		return "Ditolak";
+//	}
+//
+//}
