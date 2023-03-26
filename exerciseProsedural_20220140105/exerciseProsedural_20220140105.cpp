2ece2aeb8e66479f5f41633dd155b6091efe8fa4
giant-paw <giantpawbusiness@gmail.com>
2023-03-27 03:41:30 +0700

    penginputan nama dan nilai

--- exerciseProsedural_20220140105/exerciseProsedural_20220140105.cpp
+++ exerciseProsedural_20220140105/exerciseProsedural_20220140105.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
 using namespace std;
 
-int a[1000];
-int n;
-string nama;
-
-
 struct student {
-	string nama;
+	string nama, status;
 	double nilMat, nilFis;
 };
 
-double rerata (int i, student mahasiswa[]) {
+double rerata (int i,student mahasiswa[]) {
 	return ((mahasiswa[i].nilMat) + (mahasiswa[i].nilFis) / 2);
 }
 
-int main() {
-	
+string status1(student mahasiswa[], double rata, int nil) {
+	if (rata >= 70 || nil > 80) {
+		return "Diterima";
+	}
+	else {
+		return "Ditolak";
+	}
 }
 
+int main() {
+	const int jumlah_mahasiswa = 5;
+	student mahasiswa[jumlah_mahasiswa];
+	int i;
+	double status;
 
-//double rerata(double a, double b) {
-//	return (a + b) / 2;
-//}
-//
-//
-//string status(double rata, double nil) {
-//	if (rata >= 70 || nil > 80) {
-//		return "Diterima";
-//	}
-//	else {
-//		return "Ditolak";
-//	}
-//
-//}
+
+	//mengulang data mahasiswa yang akan dihitung
+	i = 0;
+	do {
+		cout << "=====================================" << endl;
+		cout << "\tDATA MAHASISWA KE\t " << i + 1 << endl;
+		cout << "=====================================" << endl;
+
+        cout << "Masukkan NAMA siswa ke-" << i + 1 << endl;
+        cin >> mahasiswa[i].nama;
+
+        cout << "Masukkan NILAI MATEMATIKA dari " << mahasiswa[i].nama << ": ";
+        cin >> mahasiswa[i].nilMat;
+        cout << endl;
+
+        cout << "Masukkan NILAI FISIKA dari " << mahasiswa[i].nama << ": ";
+        cin >> mahasiswa[i].nilFis;
+        cout << endl;
+
+		cout << "-------------------------------------------------------------------------------" << endl;
+		cout << "NAMA" << "\t" << "NILAI MAT" << "\t" << "NILAI FISIKA" << "\t" << "STATUS" << endl;
+		cout << "-------------------------------------------------------------------------------" << endl;
+
+		/*mahasiswa[i].status = status1();*/
+
+		cout << mahasiswa[i].nama << "\t" << mahasiswa[i].nilMat << "\t" << mahasiswa[i].nilFis << "\t" << mahasiswa[i].status << "\t";
+
+			/*if (mahasiswa[i].status || mahasiswa[i].nilMat > 80) {
+				return "Diterima";
+			}
+			else {
+				return "Ditolak";
+			}*/
+
+		cout << endl;
+		i = i + 1;
+	}
+
+	while (i < jumlah_mahasiswa);
+}
+
